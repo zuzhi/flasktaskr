ddf842d3b67761a9b062dcd2ae6828f8ee5b699d
zuzhi <476513451@qq.com>
2018-01-16 21:52:00 +0800

    Fix password should not has unique constraint

--- project/models.py
+++ project/models.py
@@ -31,7 +31,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
