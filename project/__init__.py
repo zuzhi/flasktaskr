b073c264b28ef3451d255f1a1f645250e353b741
zuzhi <476513451@qq.com>
2018-01-29 13:53:54 +0800

    Finish Task 'Building a REST API'

--- project/__init__.py
+++ project/__init__.py
@@ -16,11 +16,13 @@ bcrypt = Bcrypt(app)
 
 from project.users.views import users_blueprint
 from project.tasks.views import tasks_blueprint
+from project.api.views import api_blueprint
 
 
 # register our blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+app.register_blueprint(api_blueprint)
 
 
 @app.errorhandler(404)
