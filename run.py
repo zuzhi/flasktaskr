5f3803a6abaa5c6b63cbcac80fbd8453db4c9cf4
zuzhi <476513451@qq.com>
2019-02-26 12:16:51 +0800

    Display validation errors. And Update dependencies.

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 # run.py
-
-
 import os
 
 from project import app
 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
